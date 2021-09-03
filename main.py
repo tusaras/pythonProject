0c408e5ea0bddb263aa86e315d5dbf2a8825ad0d
Thushara Batawalage <tbatawalage@tdj.local>
2021-09-03 14:56:11 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print ("A....")
+print ("B....")
+print ("C....")
+print ("D....")
+print ("E""2""A""5" * 10)
+print (2 / 119999)
+#this os my first lesson....
+
